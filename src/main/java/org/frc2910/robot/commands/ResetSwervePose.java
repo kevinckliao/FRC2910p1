524adb2f991e4c67314c35278b35c40275340010
Sudhir Chebiyyam <schebiyyam@hotmail.com>
2025-06-17 22:56:18 -0700

    Add comments for ResetSwervePose

--- src/main/java/org/frc2910/robot/commands/ResetSwervePose.java
+++ src/main/java/org/frc2910/robot/commands/ResetSwervePose.java
@@ -3,6 +3,9 @@ package org.frc2910.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
+/**
+ * Wrapper for SequentialCommandGroup so it shows up with a different name on Shuffleboard.
+ */
 public class ResetSwervePose extends SequentialCommandGroup {
     public ResetSwervePose(Command... commands) {
         super(commands);
